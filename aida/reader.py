1390c0c96b45cf7d72155450adae53b5fead2c72
Amir Souri <a.souri@nasa.gov>
2024-05-16 06:25:33 -0700

    add glob to read ctm files properly

--- aida/reader.py
+++ aida/reader.py
@@ -620,8 +620,9 @@ def cmaq_reader_wrapper(dir_mcip: str, dir_cmaq: str, YYYYMM: str, k: int, gasna
     '''
     # finding the right files
     date = datetime.datetime.strptime(str(k), '%j').date()
-    cmaq_target_file = dir_cmaq + "/CCTM_CONC_v52_4D_" + \
-        YYYYMM[:4] + "%03d" % int(k) + ".nc"
+    cmaq_target_file = glob.glob(dir_cmaq + "/CCTM_CONC_v52*" + \
+        YYYYMM[:4] + "%03d" % int(k) + "*.nc")
+    cmaq_target_file = cmaq_target_file[0]
     grd_file_2d = dir_mcip + "/GRIDCRO2D_" + \
         YYYYMM[2:4] + date.strftime('%m%d')
     met_file_2d = dir_mcip + "/METCRO2D_" + YYYYMM[2:4] + date.strftime('%m%d')
